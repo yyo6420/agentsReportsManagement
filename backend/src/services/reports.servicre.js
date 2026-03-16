3eae37d37e51eba3dcbb8931e59b4d519757d78c
yosef ohayon <yyo6420@gmail.com>
2026-03-16 08:49:16 +0200

    validate

--- backend/src/services/reports.servicre.js
+++ backend/src/services/reports.servicre.js
@@ -3,14 +3,18 @@ import { db, reportsCollection } from "../mongodb/mongodb.js";
 import { ObjectId } from "mongodb";
 
 export const postReport = (report, userId) => {
-    const validateReport = validateReport(report);
-    validateReport.userId = userId;
-    validateReport.createdAt = new Date();
-    validateReport.sourceType = "csv"
-    return validateReport;
+    const validate = validateReport(report);
+    validate.userId = userId;
+    validate.createdAt = new Date();
+    validate.sourceType = "csv"
+    return validate;
 };
 
 export const getReportById = async (id, userId, userRole) => {
+    if (!reportsCollection) {
+        reportsCollection = db?.collection("reports");
+    }
+
     const report = await reportsCollection.findOne({ id: new ObjectId(id) });
     if (!report) throw new Error("report not found :(");
     if (userRole === "agent" && report.userId !== userId) {
